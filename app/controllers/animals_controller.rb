7ef0500db7df5422fe570ce9f58b5afb52875606
aliciarroyave <aliciarroyave1021999@gmail.com>
2021-08-30 18:58:45 -0600

    CRUD animal

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,45 @@
+class AnimalsController < ApplicationController
+    def index
+        @animals = Animal.all  
+
+        render :ok, json: @animals
+    end
+
+    def show
+        @animal = Animal.find(params[:id])
+        render 200, json: @animal
+    end
+
+    def create 
+        @animal = Animal.new(animal_params)
+
+        if @animal.save
+            render :create, json: @animal
+        else 
+            render :bad_request, json: @animal.errors
+        end
+    end    
+    def update
+        @animal = Animal.find(params[:id])
+        if @animal.update(animal_params)
+            render 200, json: @animal
+        else 
+            render 422, json: @animal.errors
+        end
+    end
+
+    def destroy
+        @animal = Animal.find(params[:id])
+        if @animal.destroy
+            render 204
+        else 
+            render 422, json: @animal.errors
+        end
+    end
+
+    protected 
+    def animal_params
+        params.require(:animal).permit(:name, :caracteristica, :descripcion, :extinto)
+    end
+end
+
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,5 @@
 class Animal < ApplicationRecord
+    validates :name, presence: true
+    validates :caracteristica, presence: true
+    validates :descripcion, presence: true
 end
