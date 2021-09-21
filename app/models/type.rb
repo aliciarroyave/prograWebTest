79d72151dc78187301e999487ac0996b517dce91
aliciarroyave <aliciarroyave1021999@gmail.com>
2021-09-20 19:32:39 -0600

    animal types controller

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,5 +2,5 @@ class Type < ApplicationRecord
     validates :name, presence: true
 
     has_many :animal_types
-    has_many :animals, through :animal_types
+    has_many :animals, through: :animal_types
 end
